cf20ff51c47f25e23e2b728dedd555a037662f2d
suresh karri <suresh42326@gmail.com>
2021-10-07 12:03:27 +0530

    unit tests

---
+++ WebApplication1.Tests/ServiceRequestSeedDataFixture.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication.Models;
+using WebApplication.Repos;
+using WebApplication1.Models;
+
+namespace WebApplication1.Tests
+{
+    public class ServiceRequestSeedDataFixture : IDisposable
+    {
+      
+        public WebApplication1Context WebApplication1Context { get; private set; }
+
+        public ServiceRequestSeedDataFixture()
+        {
+            var options = new DbContextOptionsBuilder<WebApplication1Context>()
+             .UseInMemoryDatabase(databaseName: "ServiceRequestMockDB")
+             .Options;
+            WebApplication1Context = new WebApplication1Context(options);
+            WebApplication1Context.ServiceRequest.Add(new ServiceRequest { Id = "1", CurrentStatus = CurrentStatus.Created });
+            WebApplication1Context.ServiceRequest.Add(new ServiceRequest { Id = "2", CurrentStatus = CurrentStatus.InProgress });
+            WebApplication1Context.ServiceRequest.Add(new ServiceRequest { Id = "3", CurrentStatus = CurrentStatus.Canceled });
+            WebApplication1Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            WebApplication1Context.Dispose();
+        }
+    }
+}
--- WebApplication1.Tests/ServiceRequestTest.cs
+++ WebApplication1.Tests/ServiceRequestTest.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using WebApplication.Models;
 using WebApplication.Repos;
 using WebApplication.Repos.EFCore;
 using WebApplication.Repos.Email;
@@ -18,33 +19,44 @@ using Xunit;
 
 namespace WebApplication1.Tests
 {
-    public class ServiceRequestTest
+    public class ServiceRequestTest : IClassFixture<ServiceRequestSeedDataFixture>
     {
+        ServiceRequestSeedDataFixture fixture;
+        public ServiceRequestTest(ServiceRequestSeedDataFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
         [Fact]
         public async Task GetAllTestAsync()
         {
-            var options = new DbContextOptionsBuilder<WebApplication1Context>()
-                .UseInMemoryDatabase(databaseName: "ServiceRequestMockDB")
-                .Options;
+            EFCoreServiceRequestRepo requestRepo = new EFCoreServiceRequestRepo(this.fixture.WebApplication1Context);
+            List<ServiceRequest> requests = await requestRepo.GetAll();
+            Assert.Equal(3, requests.Count);
 
-            // Insert seed data into the database using one instance of the context
-            using (var context = new WebApplication1Context(options))
-            {
-                context.ServiceRequest.Add(new ServiceRequest { Id = "1" });
-                context.ServiceRequest.Add(new ServiceRequest { Id = "2" });
-                context.ServiceRequest.Add(new ServiceRequest { Id = "3" });
-                context.SaveChanges();
-            }
-
-            // Use a clean instance of the context to run the test
-            using (var context = new WebApplication1Context(options))
-            {
-                EFCoreServiceRequestRepo requestRepo = new EFCoreServiceRequestRepo(context);
-                List<ServiceRequest> requests = await requestRepo.GetAll();
-
-                Assert.Equal(3, requests.Count);
-            }
         }
+
+        [Fact]
+        public async Task GetByIdTestAsync()
+        {
+            EFCoreServiceRequestRepo requestRepo = new EFCoreServiceRequestRepo(this.fixture.WebApplication1Context);
+            ServiceRequest request = await requestRepo.Get("1");
+            Assert.Equal("1", request.Id);
+
+        }
+
+        [Fact]
+        public async Task UpdateTestAsync()
+        {
+
+            EFCoreServiceRequestRepo requestRepo = new EFCoreServiceRequestRepo(this.fixture.WebApplication1Context);
+            ServiceRequest request = await requestRepo.Get("1");
+            request.CurrentStatus = CurrentStatus.Canceled;
+            await requestRepo.Update(request);
+            Assert.Equal(CurrentStatus.Canceled, request.CurrentStatus);
+
+        }
+
 
     }
 
